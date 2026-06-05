c8f955b0816a631b1d592b400c5dc6623895243e
Neo Yang <neoxyyang11@outlook.com>
2026-06-05 17:21:06 +1200

    Finished the html and css for the webpage, starting on the js and decided the weights for the characters

---
+++ script.js
@@ -0,0 +1,44 @@
+/*
+Hi everybody !!!! this is one of my first fluid sima 
+actually this is my first and also first project in js sooo this might be a bit ugly
+
+Im Neo and this is for hack club horizons
+This uses https://github.com/matthias-research/pages/blob/master/tenMinutePhysics/18-flip.html 
+but i changed the code quite a lot, you can find a copy of the licence below.
+
+-------------------------------------
+Copyright 2022 Matthias Müller - Ten Minute Physics, 
+www.youtube.com/c/TenMinutePhysics
+www.matthiasMueller.info/tenMinutePhysics
+
+MIT License
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+const TARGET_LONG_SIDE = 128 * 74;
+const MIN_GIRD_SIZE = 8;
+const CELL_CROP_X = 1;
+const cell_crop_Y = 2;
+
+const BASE = [
+    ["~", 12198],
+    [":", 6921],
+    ["-", 5589],
+    [".", 3267],
+    [" ", 0],
+    [" ", 0],
+
+];
+
+const RENDER_CHARS = [
+    [["N", 29420], ["N", 29420], ["n", 17950], ...BASE],
+    [["E", 25880], ["E", 25880], ["e", 18840], ...BASE],
+    [["O", 32973], ["O", 32973], ["o", 21645], ...BASE],
+    [["X", 23150], ["X", 23150], ["x", 16420], ...BASE],
+    [["Y", 19640], ["Y", 19640], ["y", 17110], ...BASE],
+]
