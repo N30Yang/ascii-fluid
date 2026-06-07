68a1cb7a71b78b91c2be8863cf88494dc98118fa
Neo Yang <neoxyyang11@outlook.com>
2026-06-08 08:22:56 +1200

    uhhh wrote down all simulation size vars

--- script.js
+++ script.js
@@ -20,25 +20,77 @@ The above copyright notice and this permission notice shall be included in all c
 THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 */
 
-const TARGET_LONG_SIDE = 128 * 74;
-const MIN_GIRD_SIZE = 8;
-const CELL_CROP_X = 1;
-const cell_crop_Y = 2;
+const target_long_side = 128 * 74;
+const min_gird_size = 8;
+const cell_crop_x = 1;
+const cell_crop_y = 2;
 
-const BASE = [
+const base = [
     ["~", 12198],
     [":", 6921],
     ["-", 5589],
     [".", 3267],
     [" ", 0],
     [" ", 0],
-
 ];
 
-const RENDER_CHARS = [
-    [["N", 29420], ["N", 29420], ["n", 17950], ...BASE],
-    [["E", 25880], ["E", 25880], ["e", 18840], ...BASE],
-    [["O", 32973], ["O", 32973], ["o", 21645], ...BASE],
-    [["X", 23150], ["X", 23150], ["x", 16420], ...BASE],
-    [["Y", 19640], ["Y", 19640], ["y", 17110], ...BASE],
-]
+const render_chars = [
+    [["N", 29420], ["N", 29420], ["n", 17950], ...base],
+    [["E", 25880], ["E", 25880], ["e", 18840], ...base],
+    [["O", 32973], ["O", 32973], ["o", 21645], ...base],
+    [["X", 23150], ["X", 23150], ["x", 16420], ...base],
+    [["Y", 19640], ["Y", 19640], ["y", 17110], ...base],
+];
+
+const canvase1 = document.getElementById("canvas");
+const rendere1 = document.querySelector(".render");
+
+const grid_size = Math.max(
+    Math.round(
+        Math.sqrt(
+            (window.innerWidth * window.innerHeight) / target_long_side
+        )
+    ),
+    min_gird_size
+);
+
+const speed_1 = 1.0 / 60.0 / 16;
+const speed_base = 1.0 / 60.0 / 3;
+const speed_2 = 1.0 / 60.0 / 1.25;
+
+const realwidth =
+    Math.ceil(window.innerWidth / grid_size + cell_crop_x * 2) *
+    grid_size;
+const realheight =
+    Math.ceil(window.innerHeight / grid_size + cell_crop_y * 2) *
+    grid_size;
+
+const y_resolution = realheight / grid_size;
+const x_resolution = realwidth / grid_size;
+const resolution = y_resolution;
+
+const gravity = 9.81;
+const extrasized = 0;
+
+canvase1.width = realwidth;
+canvase1.height = realheight;
+canvase1.style.width = realwidth + "px";
+canvase1.style.height = realheight + "px";
+rendere1.style.width = realwidth + "px";
+rendere1.style.height = realheight + "px";
+document.documentElement.style.setProperty(
+    "--cell-size",
+    grid_size + "px"
+);
+
+canvase1.focus();
+var simHeight = 2.0;
+var cscale = canvase1.height / simHeight;
+var simwidth = canvase1.width / cscale;
+var u_field = 0;
+var v_field = 1;
+var fluidcell = 0;
+var aircell = 1;
+var solidcell = 2;
+var cnt = 0;
+
