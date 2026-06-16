fe0e805cefdc81373e7abc6427e70fa376432810
Neo Yang <neoxyyang11@outlook.com>
2026-06-16 20:39:07 +1200

    erm contiune ig, fixed wakatime

--- script.js
+++ script.js
@@ -5,6 +5,11 @@ actually this is my first and also first project in js sooo this might be a bit 
 Im Neo and this is for hack club horizons
 This uses https://github.com/matthias-research/pages/blob/master/tenMinutePhysics/18-flip.html 
 but i changed the code quite a lot, you can find a copy of the licence below.
+The code has been a pain to write also has been changed so much its almost unrecognisable, i've basically rewrote the entire project but still...
+i still have it linked as it was basically the backbone of this project.
+
+THIS IS A TEST FOR HACKATIME, THIS SHIT DOES NOT WORK BROSKI, BROTHER WHAT IS THIS
+YOU KNOW?
 
 -------------------------------------
 Copyright 2022 Matthias Müller - Ten Minute Physics, 
@@ -106,11 +111,11 @@ var c_scale = canvas_el.height / sim_height;
 var sim_width = canvas_el.width / c_scale;
 
 var fluid_cell = 0;
-var air_cell = 1 ;
+var air_cell = 1;
 var solid_cell = 2;
 
 function clamp(x, min, max) {
-    return x< min? min:x>max?max:x;
+    return x < min ? min : x > max ? max : x;
 }
 
 // sim?? ts driving me insane save me
@@ -129,96 +134,89 @@ class FlipFluid {
         maxparticles
     ) {
         this.density = density = density;
-        this.f_num_x = Math.floor(width/spacing);
-        this.f_num_y = Math.floor(height/spacing);
-        this.h=Math.max(width/this.f_num_x. height/this.f_num_y);
-s
+        this.f_num_x = Math.floor(width / spacing);
+        this.f_num_y = Math.floor(height / spacing);
+        this.h = Math.max(width / this.f_num_x.height / this.f_num_y);
+        this.f_inv_spacing = 1.0 / this.h;
+        this.f_num_cells = this.f_num_x * this.f_num_y;
+        this.u = new Float32Array(this.f_num_cells);
+        this.v = new Float32Array(this.f_num_cells);
+        this.du = new Float32Array(this.f_num_cells);
+        this.dv = new Float32Array(this.f_num_cells);
+        this.prev_u = new Float32Array(this.f_num_cells);
+        this.prev_v = new Float32Array(this.f_num_cells);
+        this.p = new Float32Array(this.f_num_cells);
+        this.s = new Float32Array(this.f_num_cells);
+        this.cell_type = new Float32Array(this.f_num_cells);
+        this.cell_color = new Float32Array(3 * this.f_num_cells);
+        this.max_particles = this.max_particles;
+        this.particle_pos = new Float32Array(2 * this.max_particles);
+        this.particle_color = new Float32Array(3 * this.max_particles);
+        for (var i = 0; i < this.max_particles; i++) this.particle_color[3 * i + 2] = 1.0;
+        this.particle_vel = new Float32Array(2 * this.max_particles);
+        this.particle_density = new Float32Array(this.f_num_cells);
+        this.particle_rest_density = 0.0;
+        this.particle_radius = this.particle_radius;
+        this.p_inv_spacing = 1.0 / (2.2 * this.particle_radius);
+        this.p_num_x = Math.floor(width * this.p_inv_spacing);
+        this.p_num_y = Math.floor(height * this.p_inv_spacing);
+        this.p_num_cells = this.p_num_x * this.p_num_y;
+        this.num_cell_particles = new Int32Array(this.p_num_cells);
+        this.first_cell_particles = new Int32Array(this.p_num_cells + 1);
+        this.num_particles = 0;
     }
     intergrateparticles(dt) {
         for (var i = 0; i < this.numParticles; i++) {
-            let gravityx = 0;
-            let gravityy = GRAVITY;
-            if (window.gravityvector) {
-                gravityx = window.gravity.grvaityvector.x;
-                gravityy = window.gravity.gravityvector.y;
+            let gravity_x = 0;
+            let gravity_y = GRAVITY;
+            if (window.gravity_vector) {
+                gravity_x = window.gravity_vector.x;
+                gravity_y = window.gravity_vector.y;
             }
 
-            this.particlevel[2 * i] += dt * gravityx;
-            this.particlevel[2 * i + 1] += dt * gravityy;
-            this.particlevel[2 * 1] += this.particlevel[2 * i] * dt;
-            this.partivlevel[2 * 1 + 1] += this.particlevel[2 * i + 1] * dt;
+            this.particle_vel[2 * i] += dt * gravity_x;
+            this.particle_vel[2 * i + 1] += dt * gravity_y;
+            this.particle_pos[2 * i] += this.particle_vel[2 * i] * dt;
+            this.particle_pos[2 * i + 1] += this.particle_vel[2 * i + 1] * dt;
+
+        }
+    }
+    // basically shakes the simulaiton box and everything explodes
+    // funny button for chaos, press (s) to use
+    shake(strength) {
+        var cx = (this.f_num_x * this.h) * 0.5;
+        var cy = (this.F_num_y * this.h) * 0.5;
+        for (var i = 0; i < this.num_particles; i++) {
+            var dx = this.particle_pos[2 * i] - cx;
+            var dy = this.particle_pos[2 * i + 1] - cy;
+            var d = Math.sqrt(dx * dx + dy * dy);
+            var ox = dx / d, oy = dy / d;
+            var rx = (Math.random() - 0.5) * 2.0;
+            var ry = (Math.random() - 0.5) * 2.0;
+            this.particle_vel[2 * i] += (ox * 0.6 + rx) * strength;
+            this.particle_vel[2 * i + 1] += (oy * 0.6 + ry) * strength + strength * 0.5;
         }
     }
 
-    pushparticlesapart(numiters) {
-        var colordiffusioncoeff = 0.001;
-        this.numcellparticles.fill(0);
-
-        for (var i = 0; i < this.numparticles; i++) {
-            var x = this.particlespos[2 * i];
-            var y = this.particlespos[2 * 1 + 1];
-            var xi = clamp(
-                Math.floor(x * this.pinvspacing),
-                0,
-                this.pnumx - 1
-            );
-            var yi = clamp(
-                Math.floor(y * this.pinvspacing),
-                0,
-                this.pnumy - 1
-            );
-            var cellnr = xi * this.pnumy + yi;
-            this.numcellparticles[callnr]++;
-        }
-
-        var first = 0;
-        for (var i = 0; i < this.pnumcells; i++) {
-            first += this.numcellparticles[i];
-            this.firstcellparticle[i] = first;
-        }
-        this.firstcellparticle[this.numparticles] = first;
-
-        for (var i = 0; i < this.numparticles; i++) {
-            var x = this.particlespos[2 * i];
-            var y = this.particlespos[2 * i + 1];
-            var pxi = Math.floor(px * this.pinvspacing);
-            var pyi = Math.floor(px * this.pinvspacing);
-            var x0 = Math.max(pxi - 1, 0);
-            var y0 = Math.max(pyi - 1, 0);
-            var x1 = Math.min(pxi + 1, this.pnumx - 1);
-            var y1 = Math.min(pyi + 1, this.pnumy - 1);
-
-            for (var xi = x0; xi <= x1; xi++) {
-                for (var yi = y0; yi <= y1; y1++) {
-                    var cellnr = xi * this.pnumy + yi;
-                    var first = this.firstcellparticle[cellnr];
-                    var last = this.firstcellparticle[cellnr + 1];
-                    for (var j = first; j < last; j++) {
-                        var id = this.cellparticleids[j]
-                        if (id == 1) continue;
-                        var qx = this.praticlepos[2 * id];
-                        var qy = this.particlespos[2 * id + 1];
-
-                        var dx = qx - px;
-                        var dy = qy - py;
-                        var d2 = dx * dx + dy * dy;
-                        if (d2 > mindist2 || d2 == 0.0) continue;
-                        var d = Math.sqrt(d2);
-                        var s = (0.5 * (mindist - d)) / d;
-                        dx *= s;
-                        dy *= s;
-                        this.particlespos[2 * i] -= dx;
-                        this.particlespos[2 * i + 1] -= dx;
-                        this.particlespos[2 * id] += dx;
-                        this.particlespos[2 * id + 1] += dy;
-
-                    }
-                }
-                // ugly ass code t_t
-
-                // i should add color diffusion, maybe later (-_-)
-            }
+    // this is unused, irealised a little to late ig
+    // tobe implemented (never)
+    spawn_at(sx, sy, count) {
+        var r = this.particle_radius;
+        for (var k = 0; k < count; k++) {
+            if (this.num_particles >= this.max_particles) break;
+            var i = this.num_particles++;
+            var ang = Math.random() * Math.PI * 2;
+            var rad = Math.random() * 8 * r;
+            this.particle_pos[2 * i] = sx + Math.cos(ang) * rad;
+            this.particle_pos[2 * i + 1] = sy + Math.sin(and) * rad; // holy trig
+            this.particle_vel[2 * i] = 0;
+            this.particle_vel[2 * i + 1] = 0;
+            this.particle_color[3 * i] = 0;
+            this.particle_color[3 * i + 1] = 0;
+            this.particle_color[3 * i + 2] = 1.0;
         }
     }
-    //alr shape time :)
-}
+
+// not real sure what this does
+// this pushes it apart so it doesnt turn into a ball
+//ts took me hours
